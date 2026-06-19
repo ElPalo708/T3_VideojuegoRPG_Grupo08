782e7407ed353a77bc5ff652c285862e415999c2
andersonbsc22 <andersonbsc22@gmail.com>
2026-06-19 02:48:37 -0500

    Acoplamiento de la historia en el flujo principal del programa

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using T3_VideojuegoRPG_Grupo08.Modelos;
+using T3_VideojuegoRPG_Grupo08.Mapa;
+using T3_VideojuegoRPG_Grupo08.Sistema;
+
+Console.Clear();
+Console.WriteLine("=== BIENVENIDO A SWORD ART ONLINE ===");
+Console.WriteLine("Estas atrapado en el juego. Si mueres aqui, mueres en la vida real.");
+Console.WriteLine("Elige tu camino inicial:");
+Console.WriteLine("1. Ir por el bosque oscuro.");
+Console.WriteLine("2. Ir por las ruinas antiguas.");
+Console.Write("Selecciona (1 o 2): ");
+
+string opcion = Console.ReadLine();
+string zonaActual = "";
+
+if (opcion == "1")
+{
+    zonaActual = "BOSQUE OSCURO";
+    Console.WriteLine("\nElegiste el bosque. Avanzas con cuidado entre los arboles...");
+}
+else
+{
+    zonaActual = "RUINAS ANTIGUAS";
+    Console.WriteLine("\nElegiste las ruinas. Hay peligro por todos lados...");
+}
+Console.WriteLine("Presiona una tecla para ingresar al mapa...");
+Console.ReadKey(true);
+
+EncuentrosRandom encuentros = new EncuentrosRandom();
+
+Jugador jugador = new Jugador
+{
+    PosX = 1,
+    PosY = 1
+};
+
+Mapa mapa = new Mapa();
+
+while (true)
+{
+    Historia.RevisarFinJuego(jugador);
+
+    mapa.Dibujar(jugador);
+    
+    Console.WriteLine($"\n📍 REGIÓN: {zonaActual}");
+    Console.WriteLine($" Nivel: {jugador.Nivel} | Vida: {jugador.VidaActual}/{jugador.VidaMaxima} | Oro: {jugador.Oro}/200 | EXP: {jugador.ExpActual}/{jugador.ExpRequerida}");
+
+    ConsoleKey tecla = Console.ReadKey(true).Key;
+
+    int nuevoX = jugador.PosX;
+    int nuevoY = jugador.PosY;
+
+    switch (tecla)
+    {
+        case ConsoleKey.W: nuevoY--; break;
+        case ConsoleKey.S: nuevoY++; break;
+        case ConsoleKey.A: nuevoX--; break;
+        case ConsoleKey.D: nuevoX++; break;
+    }
+
+    mapa.MoverJugador(jugador, nuevoX, nuevoY);
+
+    if(encuentros.VerificarEncuentro())
+    {
+        Console.WriteLine("\n¡Has encontrado un enemigo!");
+        Console.WriteLine("Presiona cualquier tecla para continuar...");
+        Console.ReadKey(true);
+    }
+}
