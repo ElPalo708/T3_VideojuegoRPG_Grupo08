f96793f471f02bc87ae6b6bee1c958260351417b
andersonbsc22 <andersonbsc22@gmail.com>
2026-06-19 05:16:45 -0500

    intro y final

--- Program.cs
+++ Program.cs
@@ -2,11 +2,7 @@
 using T3_VideojuegoRPG_Grupo08.Mapa;
 using T3_VideojuegoRPG_Grupo08.Sistema;
 
-
-Console.WriteLine("=== RPG GRUPO 08 ===");
-Console.WriteLine("Pulsa una tecla para comenzar...");
-Console.ReadKey(true);
-Console.Clear();
+Historia.MostrarIntro();
 
 EncuentrosRandom encuentros = new EncuentrosRandom();
 
@@ -20,6 +16,8 @@ Mapa mapa = new Mapa();
 
 while (true)
 {
+    Historia.RevisarFinJuego(jugador);
+
     mapa.Dibujar(jugador);
 
     ConsoleKey tecla = Console.ReadKey(true).Key;
@@ -48,12 +46,12 @@ while (true)
 
     mapa.MoverJugador(jugador, nuevoX, nuevoY);
 
-if (encuentros.VerificarEncuentro())
-{
-    Console.WriteLine("¡Has encontrado un enemigo!");
-    Console.ReadKey(true);
+    if (encuentros.VerificarEncuentro())
+    {
+        Console.WriteLine("¡Has encontrado un enemigo!");
+        Console.ReadKey(true);
 
-    Combate combate = new Combate();
-    combate.Iniciar(jugador);
-}
-}   
+        Combate combate = new Combate();
+        combate.Iniciar(jugador);
+    }
+}  
